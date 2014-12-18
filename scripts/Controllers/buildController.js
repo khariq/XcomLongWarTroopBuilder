7f6889348260c4ac7381e33927da321d16d3e7fa
Michael Pickens <khariq@gmail.com>
2014-12-17 23:24:37 -0500

    First pass at Menu system

---
+++ scripts/Controllers/buildController.js
@@ -0,0 +1,26 @@
+﻿
+xcomApp.controller('buildController', function ($scope, $http) {
+
+    $scope.perks = [];
+    $scope.ranks = [];
+    $scope.selectedMenuItem = "Assault";
+    if (commonJson == null) {
+        $http.get('data/common.json').success(function (data) {
+            commonJson = data;
+            $scope.perks = commonJson.perks;
+            $scope.ranks = commonJson.ranks;
+        });
+    }
+    else {
+        $scope.perks = commonJson.perks;
+        $scope.ranks = commonJson.ranks;
+    }
+
+    
+});
+
+function init ($scope) {
+    $scope.perks = commonJson.perks;
+    $scope.ranks = commonJson.ranks;
+}
+
--- scripts/Controllers/navController.js
+++ scripts/Controllers/navController.js
@@ -1,8 +1,13 @@
 
-xcomApp.controller('navController', function($scope) {
-	$.getJSON('data/common.json', function(json) { 
-		commonJson = JSON.parse(json); 
-		
-		$scope.menuItems = commonJson.classes;
-	});
+xcomApp.controller('navController', function($scope, $http) {
+
+    if (commonJson == null) {
+        $http.get('data/common.json').success(function (data) {
+            commonJson = data;
+            $scope.menuItems = commonJson.classes;
+        });
+    }
+    else {
+        $scope.menuItems = commonJson.classes;
+    }
 });
